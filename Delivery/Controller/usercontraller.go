b0e9027417cc1d9a98ea3ba22465643a3187a01a
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-27 12:00:10 +0300

    chore: Add Config package and UserController to handle user-related operations

--- Delivery/Controller/usercontraller.go
+++ Delivery/Controller/usercontraller.go
@@ -1,1 +1,110 @@
 package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	domain "github.com/legend123213/loan_managment/Domain"
+	usecase "github.com/legend123213/loan_managment/Usecase"
+)
+
+type UserController struct {
+	UserUsecase usecase.UserServiceUsecase
+}
+
+	
+
+func NewUserController(usecase usecase.UserServiceUsecase) *UserController {
+	return &UserController{
+		UserUsecase: usecase,
+	}
+}
+
+func (controller *UserController) CreateUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		var user *domain.User // Change the type to a pointer to domain.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}		
+		user, err := controller.UserUsecase.AddUser(user) // Remove the address-of operator
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, gin.H{
+			"user": user,
+		})
+	}
+}
+
+func (controller *UserController) GetUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		id := c.Param("id")
+		user, err := controller.UserUsecase.GetUser(id)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, gin.H{
+			"user": user,
+		})
+	}
+}
+
+// Remove the duplicate declaration of GetUsers method
+
+func (controller *UserController) UpdateUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		var user *domain.User // Change the type to a pointer to domain.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}		
+		user, err := controller.UserUsecase.UpdateUser(user) // Remove the address-of operator
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, gin.H{
+			"user": user,
+		})
+	}
+}
+
+func (controller *UserController) DeleteUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		id := c.Param("id")
+		err := controller.UserUsecase.DeleteUser(id)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, gin.H{
+			"message": "User deleted successfully",
+		})
+	}
+}
+
+func (controller *UserController) FatchUsers() gin.HandlerFunc{
+	return func(c *gin.Context){
+		users, err := controller.UserUsecase.GetUsers()
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		c.JSON(200, gin.H{
+			"users": users,
+		})
+	}
+}
--- Delivery/main.go
+++ Delivery/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"context"
+	"log"
+
+	config "github.com/legend123213/loan_managment/Config"
+	controller "github.com/legend123213/loan_managment/Delivery/Controller"
+	routes "github.com/legend123213/loan_managment/Delivery/Routes"
+	repositery "github.com/legend123213/loan_managment/Repositery"
+	usecase "github.com/legend123213/loan_managment/Usecase"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//mongo connection
+func mongoconnection() *mongo.Client {
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+	// mongo connection
+	client, err := mongo.NewClient(options.Client().ApplyURI(config.Database.Uri))
+	if err != nil {
+		log.Fatalf("error creating mongo client: %v", err)
+	}
+	if err := client.Connect(context.Background()); err != nil {
+		log.Fatalf("error connecting to mongo: %v", err)
+	}
+	return client
+	
+}
+func main() {
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("error loading config: %v", err)
+	}
+	mongoClint := mongoconnection()
+	userRepo := repositery.NewUserServiceRepo(mongoClint.Database(config.Database.Name))
+	userUsecase := usecase.NewUserServiceUsecase(*userRepo)
+	userController := controller.NewUserController(*userUsecase)
+	mainRoute :=routes.NewRoute(userController)
+	mainRoute.SetupRouter().Run(config.Port)
+
+}
