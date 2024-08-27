0effe125d2722d39c0d1081e3a36c17b893e2ed6
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-27 12:00:18 +0300

    chore: Add route setup function to handle user-related routes

--- Delivery/Routes/route.go
+++ Delivery/Routes/route.go
@@ -1,1 +1,30 @@
 package routes
+
+// gin-gonic
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/legend123213/loan_managment/Delivery/Controller"
+)
+type Route struct {
+	UserController *controller.UserController
+}
+
+func NewRoute(userController *controller.UserController) *Route {
+	return &Route{
+		UserController: userController,
+	}
+}
+
+func (route *Route) SetupRouter() *gin.Engine {
+	r := gin.Default()
+	r.Use(gin.Recovery())
+	r.POST("/users", route.UserController.CreateUser())
+	r.GET("/users/:id", route.UserController.GetUser())
+	r.GET("/users", route.UserController.FatchUsers())
+	r.PUT("/users", route.UserController.UpdateUser())
+	r.DELETE("/users/:id", route.UserController.DeleteUser())
+	return r
+}
+
+
+
